d8dfd4c097384e228b66113d2de174e7cab68fa3
Anikate Kanoo <anikate955@gmail.com>
2026-03-15 14:09:54 +0530

    add project page

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
+import NavBar from './Navigation/NavBar'
+import FullScreenNav from './Navigation/FullScreenNav'
 
 
 const App = () => {
   
-
-  
-  
   return (
     <div className='text-white'>
+      <NavBar />
+      <FullScreenNav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agence" element={<Agence />} />
---
+++ src/Navigation/FullScreenNav.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div className="h-screen w-full absolute bg-black py-50">
+      <div className=''>
+        <div className='link border-y-1 relative ' >
+            <div>
+              <h1 className='font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>PROJETS</h1>
+            </div>
+
+            <div className='movelink absolute flex text-black top-0 bg-[#D3FD50]'>
+                <div className='moveX shrink-0 flex item-center'>
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/PJC_SiteK72_Thumbnail_640x290.jpg" alt="" />
+
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/Thumbnail.png" alt="" />
+                </div>
+
+                <div className='moveX shrink-0 flex item-center'>
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/PJC_SiteK72_Thumbnail_640x290.jpg" alt="" />
+
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/Thumbnail.png" alt="" />
+                </div>
+            </div>
+
+        </div>
+
+        <div className='link border-y-1 relative ' >
+            <div>
+              <h1 className='font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>PROJETS</h1>
+            </div>
+
+            <div className='movelink absolute flex text-black top-0 bg-[#D3FD50]'>
+                <div className='moveX shrink-0 flex item-center'>
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/PJC_SiteK72_Thumbnail_640x290.jpg" alt="" />
+
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/Thumbnail.png" alt="" />
+                </div>
+
+                <div className='moveX shrink-0 flex item-center'>
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/PJC_SiteK72_Thumbnail_640x290.jpg" alt="" />
+
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/Thumbnail.png" alt="" />
+                </div>
+            </div>
+
+        </div>
+
+        <div className='link border-y-1 relative ' >
+            <div>
+              <h1 className='font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>PROJETS</h1>
+            </div>
+
+            <div className='movelink absolute flex text-black top-0 bg-[#D3FD50]'>
+                <div className='moveX shrink-0 flex item-center'>
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/PJC_SiteK72_Thumbnail_640x290.jpg" alt="" />
+
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/Thumbnail.png" alt="" />
+                </div>
+
+                <div className='moveX shrink-0 flex item-center'>
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/PJC_SiteK72_Thumbnail_640x290.jpg" alt="" />
+
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/Thumbnail.png" alt="" />
+                </div>
+            </div>
+
+        </div>
+
+        <div className='link border-y-1 relative ' >
+            <div>
+              <h1 className='font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>PROJETS</h1>
+            </div>
+
+            <div className='movelink absolute flex text-black top-0 bg-[#D3FD50]'>
+                <div className='moveX shrink-0 flex item-center'>
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/PJC_SiteK72_Thumbnail_640x290.jpg" alt="" />
+
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/Thumbnail.png" alt="" />
+                </div>
+
+                <div className='moveX shrink-0 flex item-center'>
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/PJC_SiteK72_Thumbnail_640x290.jpg" alt="" />
+
+                    <h2 className='whitespace-nowrap font-[font1] text-[8vw] text-center leading-[0.8] pt-5'>POUR TOUT VOIR</h2>
+
+                    <img className='mt-2 h-26 w-72 rounded-full object-cover' src="/Nav/Thumbnail.png" alt="" />
+                </div>
+            </div>
+
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
